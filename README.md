9acd038d528f4b13b664b6ee63f18dfe67117c1d
Azeem, Aamer <aamer.azeem@vanderbilt.edu>
2020-01-08 23:16:02 -0600

    Adding a basic palindrome string checker

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # java-examples
+Palindrome: A palindrome string is a string that is same after reverse.
+Ex: abba
---
+++ src/main/java/org/java/examples/Palindrome.java
@@ -0,0 +1,21 @@
+package org.java.examples;
+
+
+public class Palindrome {
+   
+   public static void main(String[] args) {
+      if(isPalindrome( "abba" )) {
+         System.out.println("The string is a palindrome");
+      } else {
+         System.out.println("The string is not a palindrome");
+      }
+      
+   }
+
+   public static boolean isPalindrome(String str) {    
+      int n = str.length();
+      for( int i = 0; i < n/2; i++ )
+          if (str.charAt(i) != str.charAt(n-i-1)) return false;
+      return true;    
+  }
+}
